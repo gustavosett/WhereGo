7b6243b2cd6539b1729aba48a52b50c34fc5d937
Gustavo Carvalho <gustaavoribeeiro@hotmail.com>
2025-11-26 19:00:29 -0300

    chore: simplify service structure

--- internal/geoip/service.go
+++ internal/geoip/service.go
@@ -3,7 +3,6 @@ package geoip
 import (
 	"errors"
 	"net/netip"
-	"sync"
 
 	"github.com/oschwald/geoip2-golang"
 )
@@ -11,8 +10,7 @@ import (
 var ErrInvalidIP = errors.New("invalid IP address")
 
 type Service struct {
-	DB   *geoip2.Reader
-	pool sync.Pool
+	DB *geoip2.Reader
 }
 
 func NewService(dbPath string) (*Service, error) {
@@ -20,48 +18,33 @@ func NewService(dbPath string) (*Service, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Service{
-		DB: db,
-		pool: sync.Pool{
-			New: func() any {
-				return &LookupData{}
-			},
-		},
-	}, nil
+	return &Service{DB: db}, nil
 }
 
-type LookupData struct {
-	Country  string
-	City     string
-	ISOCode  string
-	Timezone string
+type LookupResult struct {
+	IP       string `json:"ip"`
+	Country  string `json:"country"`
+	City     string `json:"city"`
+	ISOCode  string `json:"iso_code"`
+	Timezone string `json:"timezone"`
 }
 
-// GetLookupData gets a LookupData from the pool
-func (s *Service) GetLookupData() *LookupData {
-	return s.pool.Get().(*LookupData)
-}
-
-// PutLookupData returns a LookupData to the pool
-func (s *Service) PutLookupData(d *LookupData) {
-	s.pool.Put(d)
-}
-
-func (s *Service) LookupIP(ipStr string, data *LookupData) error {
+func (s *Service) LookupIP(ipStr string) (*LookupResult, error) {
 	addr, err := netip.ParseAddr(ipStr)
 	if err != nil {
-		return ErrInvalidIP
+		return nil, ErrInvalidIP
 	}
 
 	record, err := s.DB.City(addr.AsSlice())
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	data.Country = record.Country.Names["en"]
-	data.City = record.City.Names["en"]
-	data.ISOCode = record.Country.IsoCode
-	data.Timezone = record.Location.TimeZone
-
-	return nil
+	return &LookupResult{
+		IP:       ipStr,
+		Country:  record.Country.Names["en"],
+		City:     record.City.Names["en"],
+		ISOCode:  record.Country.IsoCode,
+		Timezone: record.Location.TimeZone,
+	}, nil
 }
