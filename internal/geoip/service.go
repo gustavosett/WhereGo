80bb8e64330b309da7c6f17edc5c8f168cc4305b
Gustavo Carvalho <gustaavoribeeiro@hotmail.com>
2025-12-06 23:59:21 -0300

    feat: Implement GeoIP2 database reader

--- internal/geoip/service.go
+++ internal/geoip/service.go
@@ -3,29 +3,27 @@ package geoip
 import (
 	"errors"
 	"net/netip"
-
-	"github.com/oschwald/geoip2-golang"
 )
 
 var ErrInvalidIP = errors.New("invalid IP address")
 
 type Service struct {
-	DB *geoip2.Reader
+	DB *Reader
 }
 
 func NewService(dbPath string) (*Service, error) {
-	db, err := geoip2.Open(dbPath)
+	db, err := Open(dbPath)
 	if err != nil {
 		return nil, err
 	}
 	return &Service{DB: db}, nil
 }
 
-func (s *Service) LookupIP(ipStr string) (*geoip2.City, error) {
+func (s *Service) LookupIP(ipStr string) (*City, error) {
 	addr, err := netip.ParseAddr(ipStr)
 	if err != nil {
 		return nil, ErrInvalidIP
 	}
 
-	return s.DB.City(addr.AsSlice())
+	return s.DB.City(addr)
 }
